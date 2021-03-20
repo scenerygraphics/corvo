4dc588e987284a94ba61a4aaa1aacdcd485143f4
luke <ljjh20@bath.ac.uk>
2021-03-19 17:51:23 -0700

    file_converter.py now both adds the 3d umap and recompressed X into a csc and stores it in a layer. It now also calls SelectionsToCSV within it.

--- file_conversion/file_converter.py
+++ file_conversion/file_converter.py
@@ -1,23 +1,31 @@
 import scanpy as sc
-from file_converter_test_2d import FileConverterTest2D
-import h5py
+from file_conversion.test.file_converter_test_2d import FileConverterTest2D
+from selections_to_csv import SelectionsToCSV
 
 
 class FileConverter:
     """
 """
     def __init__(self):
-        original_file = "file_conversion/tabula-muris-senis-facs-processed-official-annotations-Aorta.h5ad"
-        results_file = "file_conversion/temporary.h5ad"
+        original_file = "file_conversion/tabula-muris-senis-facs-processed-official-annotations-Liver.h5ad"
+        results_file = "file_conversion/liver_vr_processed.h5ad"
 
         # file to add 3d umap to
-        adata = sc.read_h5ad(original_file, True)
+        adata = sc.read_h5ad(original_file)
 
         adata.obsm['X_umap_2d'] = adata.obsm['X_umap'].copy()
         adata.uns['neighbors']['params']['n_pcs'] = adata.uns['neighbors']['params']['n_pcs'][0]
-        sc.tl.umap(adata, 3)
 
-        FileConverterTest2D(original_file, results_file)
+        sc.tl.umap(adata, n_components=3)
+
+        # adding csc layer
+        adata.layers["X_csc"] = adata.X.tocsc()
+        adata.write(results_file)
+
+        # FileConverterTest2D(original_file, results_file)
+
+        # annotations to csv
+        SelectionsToCSV(results_file, "liver")
 
 
 if __name__ == "__main__":
