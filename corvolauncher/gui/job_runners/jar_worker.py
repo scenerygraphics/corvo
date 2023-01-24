565efd7580618a9bad87b5e1d3fe60ff649f4882
Ulrik Guenther <hello@ulrik.is>
2023-01-24 17:09:49 +0100

    JarWorker: Correctly use and extract scenery/corvo JAR file

--- corvolauncher/gui/job_runners/jar_worker.py
+++ corvolauncher/gui/job_runners/jar_worker.py
@@ -2,6 +2,7 @@ import os
 import time
 from pathlib import Path
 from subprocess import Popen, DEVNULL, STDOUT
+from importlib.resources import files
 
 from PyQt5.QtCore import QRunnable, pyqtSlot, pyqtSignal, QObject
 
@@ -12,6 +13,7 @@ class JarWorker(QRunnable):
     def __init__(self, file_name, *args, **kwargs):
         super(JarWorker, self).__init__()
 
+        self.jar_name = "corvo-0.1.0-SNAPSHOT-all.jar"
         self.args = args
         self.kwargs = kwargs
         self.signals = WorkerSignals()
@@ -24,9 +26,21 @@ class JarWorker(QRunnable):
     def run(self):
         try:
             self.signals.running.emit()
-            bash_command = "java -jar corvo-0.1.0-SNAPSHOT-all.jar " + "processed_datasets/" + self.file_name + \
+
+            base_path = os.path.join(str(Path.home()), ".corvo", "resources")
+            jar_path = os.path.join(base_path, self.jar_name)
+            if not os.path.exists(jar_path):
+                print("Extracing scenery JAR, this is a one-time process...")
+                jar_contents = files('corvolauncher').joinpath(self.jar_name).read_bytes()
+                jar_file = open(jar_path, "wb")
+                jar_file.write(jar_contents)
+                jar_file.close()
+                print("Extraction done, launching Corvo.")
+
+            bash_command = "java -cp " + self.jar_name + " graphics.scenery.corvo.XVisualization processed_datasets/" + self.file_name + \
                            " vosk-model-small-en-us-0.15/vosk-model-small-en-us-0.15"
-            process = Popen(bash_command.split(), cwd=os.path.join(str(Path.home()), ".corvo", "resources"))
+            print("Launching " + bash_command + " in directory " + base_path)
+            process = Popen(bash_command.split(), cwd=base_path)
 
             while not self.shutdown:
                 time.sleep(0.1)
